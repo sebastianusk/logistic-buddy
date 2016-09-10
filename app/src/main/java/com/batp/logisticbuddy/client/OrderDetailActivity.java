b43bc9cd3ea29040bffae446350d1a27a9039ddf
nisie <nathania.sutedja@tokopedia.com>
2016-09-10 21:08:22 +0530

    Add order detail activity

--- app/src/main/java/com/batp/logisticbuddy/client/OrderDetailActivity.java
+++ app/src/main/java/com/batp/logisticbuddy/client/OrderDetailActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.batp.logisticbuddy.R;
 
-public class DriverDetailActivity extends AppCompatActivity {
+public class OrderDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_detail);
+        setContentView(R.layout.activity_order_detail);
     }
 }
