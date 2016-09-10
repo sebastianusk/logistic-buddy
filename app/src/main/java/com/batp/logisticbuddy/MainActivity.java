10e1f9dba6df4359b31da70980c1d0366a1f63b2
Sebastianus Kurniawan HS <sebastianus.kurniawan@tokopedia.com>
2016-09-10 11:51:43 +0530

    init abstract map activity

--- app/src/main/java/com/batp/logisticbuddy/MainActivity.java
+++ app/src/main/java/com/batp/logisticbuddy/MainActivity.java
@@ -3,6 +3,8 @@ package com.batp.logisticbuddy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/batp/logisticbuddy/map/BaseMapActivity.java
@@ -0,0 +1,57 @@
+package com.batp.logisticbuddy.map;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+import com.batp.logisticbuddy.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+
+/**
+ * Created by Toped18 on 9/10/2016.
+ */
+public abstract class BaseMapActivity  extends Activity implements OnMapReadyCallback {
+
+    private static final int PERMISSION_ACCESS_FINE_LOCATION = 10000;
+    private GoogleMap mMap;
+    private UiSettings uiSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutId());
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    protected abstract int getLayoutId();
+    protected abstract UiSettings setMapUISetting(GoogleMap googleMap);
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setMapUISetting(googleMap);
+        setLocationEnabled();
+    }
+
+    private void setLocationEnabled() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_ACCESS_FINE_LOCATION);
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+}
