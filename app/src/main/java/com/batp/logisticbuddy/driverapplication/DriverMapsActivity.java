583ccba6db51f3409a6883c3f08d1a77ff0b7b2f
Christian <christian.daniel@tokopedia.com>
2016-09-11 12:09:31 +0530

    increase the ringtone threshold

--- app/src/main/java/com/batp/logisticbuddy/driverapplication/DriverMapsActivity.java
+++ app/src/main/java/com/batp/logisticbuddy/driverapplication/DriverMapsActivity.java
@@ -412,7 +412,7 @@ public class DriverMapsActivity extends BaseMapActivity implements SpeedingResul
                         * Double.parseDouble(resultData.getString(DriverIntentService.SENSOR_Z_AXIS, "0"))
                 );
 
-                if(vector > 25) {
+                if(vector > 30) {
                     Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                     Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
                     r.play();
